75aca2c87c75134890c8cb33242a20c77bb16551
Alexander Kluber <ajkluber@rice.edu>
2014-05-23 18:54:54 -0500

    Debugging the calculated of MC2004 equation (9)

--- mutations/mutatepdbs.py
+++ mutations/mutatepdbs.py
@@ -85,6 +85,8 @@ def get_core_mutations():
     mutation_data = np.loadtxt("calculated_ddG.dat",dtype=str)
     useable_and_core = np.array([ all([(mutation_data[i,0] == "core"), bool(mutation_data[i,8])]) for i in range(mutation_data.shape[0]) ])
     
+    print useable_and_core
+    raise SystemExit
     mut_indx = mutation_data[(useable_and_core == True),1] 
     wt_res = mutation_data[(useable_and_core == True),2] 
     mut_res = mutation_data[(useable_and_core == True),3] 
