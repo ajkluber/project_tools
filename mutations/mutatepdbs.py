222470671cbbd5ae83002e6e12afb8fdd205c187
Alexander Kluber <ajkluber@rice.edu>
2014-05-23 19:15:22 -0500

    This error  bool('False')=True

--- mutations/mutatepdbs.py
+++ mutations/mutatepdbs.py
@@ -85,11 +85,12 @@ def get_core_mutations():
     mutation_data = np.loadtxt("calculated_ddG.dat",dtype=str)
     useable_and_core = []
     for i in range(mutation_data.shape[0]):
-        if (mutation_data[i,0] == "core") and bool(mutation_data[i,8]):
+        if (mutation_data[i,0] == "core") and (mutation_data[i,8] == "True"):
             useable_and_core.append(True)
         else:
             useable_and_core.append(False)
             
+    useable_and_core = np.array(useable_and_core)
     #useable_and_core = np.array([ all([(mutation_data[i,0] == "core"), bool(mutation_data[i,8])]) for i in range(mutation_data.shape[0]) ])
 
     mut_indx = mutation_data[(useable_and_core == True),1] 
@@ -112,10 +113,11 @@ def get_core_mutation_ddG():
     #useable_and_core = np.array([ all([(mutation_data[i,0] == "core"), bool(mutation_data[i,8])]) for i in range(mutation_data.shape[0]) ])
     useable_and_core = []
     for i in range(mutation_data.shape[0]):
-        if (mutation_data[i,0] == "core") and  bool(mutation_data[i,8]):
+        if (mutation_data[i,0] == "core") and (mutation_data[i,8] == "True"):
             useable_and_core.append(True)
         else:
             useable_and_core.append(False)
+    useable_and_core = np.array(useable_and_core)
     
     ddG_N_D = mutation_data[(useable_and_core == True),4] 
     ddG_N_D_err = mutation_data[(useable_and_core == True),5] 
