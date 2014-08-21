5baa77cc2770555679b0e0fc166f5a124455f8d2
Alexander Kluber <ajkluber@rice.edu>
2014-08-21 17:25:29 -0500

    Reorganized parameter_fitting into submodules

--- parameter_fitting/__init__.py
+++ parameter_fitting/__init__.py
@@ -1,6 +1,2 @@
-import mutatepdbs
-import phi_values
-import perturbation
-import prepare_ddG
-import branch
-import flip_beadbead
+import ddG_MC2004
+import newton
---
+++ parameter_fitting/ddG_MC2004/__init__.py
@@ -0,0 +1,6 @@
+import mutatepdbs
+import phi_values
+import perturbation
+import prepare_ddG
+import branch
+import flip_beadbead
