a75450dfa9cc792309b43c136baaefc978212e8a
Justin Chen <jchen978@gmail.com>
2014-12-03 15:55:05 -0600

    parameter_fitting/FRET/compute_Jacobian updated

--- parameter_fitting/FRET/compute_Jacobian.py
+++ parameter_fitting/FRET/compute_Jacobian.py
@@ -8,7 +8,7 @@ such as measured with FRET.
 
 note: as of now, only compute distances for FRET is updated
 
-last updated: Justin Chen, November 21, 2014
+last updated: Justin Chen, November 26, 2014
 
 
 """
@@ -26,117 +26,208 @@ from project_tools.parameter_fitting.util.util import *
 
 global GAS_CONSTANT_KJ_MOL
 GAS_CONSTANT_KJ_MOL = 0.0083144621
+FRET_pairs = [[114,192]]
+def_temp = 135
+defspacing = 0.1 ## in nm
 
-def get_state_bounds():
-    """ Bounds for each state. Bounds are bin edges along Q. """
-    if os.path.exists("state_bounds.txt"):
-        statefile = open("state_bounds.txt","r").readlines()
-    else:
-        print "ERROR!"
-        print "  Please create state_bounds.txt"
-        print "  With the boundaries of each state along Q"
-        print "  Exiting"
-        raise SystemExit
-    
-    state_bounds = []
-    state_labels = []
-    for line in statefile:
-        info = line.split()
-        state_labels.append(info[0])
-        state_bounds.append(float(info[1]))
-        state_bounds.append(float(info[2]))
-    
-    return state_bounds,state_labels
-
-def get_target_feature(model):
-    """ Get target features """
-    name = model.subdir
-    iteration = model.Mut_iteration
+def calc_sim_bins(model,residues=FRET_pairs,fit_temp=def_temp,spacing=defspacing):
+    """calc_sim_bins calculates and writes the simulation files """
+    ##assumes you are in the folder containing the model subdir
+    print "Calculating Simulation FRET bins"
     cwd = os.getcwd()
-    sub = "%s/%s/Mut_%d" % (cwd,name,iteration)
-    
-
-    if os.path.exists("%s/target_Qi.dat" % name):
-        target =  np.loadtxt("%s/target_Qi.dat" % name)
-        target_err =  np.loadtxt("%s/target_Qi_err.dat" % name)
-    else:
-        ## Compute the average Q of the TS: Average of the endpoints.
-        os.chdir("%s" % sub)
-        bounds, state_labels = get_state_bounds()
-        Q_TS = 0.5*(bounds[2] + bounds[3])/float(model.n_contacts)
-        target = Q_TS*np.ones(model.n_contacts,float)
-        target_err = 0.05*np.ones(model.n_contacts,float)
-        os.chdir(cwd)
-
-    return target, target_err
-'''
-def get_rij_Vij(model):
-    """ Load trajectory, state indicators, and contact energy """
-    ##assumes you are in the directory with traj.xtc and Native.pdb
-    traj = md.load("traj.xtc",top="Native.pdb")     ## Loading from file takes most time.
-    ## rij is a matrix, where first index represents trajectory step, and the second index represents the different pairs
-    rij = md.compute_distances(traj,model.contacts-np.ones(model.contacts.shape),periodic=False)
-    
-
-    Vp = np.zeros((traj.n_frames,model.n_contacts),float)
-    for i in range(model.n_contacts):       ## loop over number of parameters
-        param_idx = model.pairwise_param_assignment[i]
-        Vp[:,param_idx] = Vp[:,param_idx] + model.pairwise_potentials[i](rij[:,i])
-    Vij = Vp
-
-    
-    Vij = model.calculate_contact_potential(rij)
-    return traj,rij,Vij
-'''
-def calculate_average_Jacobian(model,residues):
-    """ Calculate the average feature vector (ddG's) and Jacobian """
-    
-    name = model.subdir
+    subdir = model.subdir
     iteration = model.Mut_iteration
-
-    cwd = os.getcwd()
-    sub = "%s/%s/Mut_%d" % (cwd,name,iteration)
-    os.chdir(sub)
-
-    temperatures = [ x.split('_')[0] for x in open("T_array_last.txt","r").readlines() ] 
-    directories = [ x.rstrip("\n") for x in open("T_array_last.txt","r").readlines() ] 
-
-    #bounds, state_labels = get_state_bounds()
-    #bounds = [0] + bounds + [model.n_contacts]
-
-    ## Loop over temperatures in Mut subdir. Calculate ddG vector and 
-    ## Jacobian for each directory indpendently then save. 
-    sim_feature_all = []
-    Jacobian_all = []
-    lasttime = time.time()
-    for n in range(len(directories)):
-        T = temperatures[n]
-        dir = directories[n]
-        beta = 1./(GAS_CONSTANT_KJ_MOL*float(T))
-        print "  Calculating Jacobian for Mut_%d/%s" % (model.Mut_iteration,dir)
-        os.chdir(dir)
-        sim_feature, Jacobian = compute_Jacobian_for_directory(model,beta,residues)
-        sim_feature_all.append(sim_feature)
-        Jacobian_all.append(Jacobian)
-        os.chdir("..")
-
-        thistime = time.time()
-        timediff = thistime - lasttime
-        lasttime = thistime
-        print "  calculation took %.2f seconds = %.2f minutes" % (timediff,timediff/60.)
-
-    sim_feature_all = np.array(sim_feature_all)
-    Jacobian_all = np.array(Jacobian_all)
-
-    ## Take avg. and use standard deviation as error bars.
-    sim_feature_avg = sum(sim_feature_all)/float(len(directories))
-    sim_feature_err = np.std(sim_feature_all,axis=0)
-    Jacobian_avg = sum(Jacobian_all)/float(len(directories))
-    Jacobian_err = np.std(Jacobian_all,axis=0)
-
+    
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subtemp = "%s/%d_0" % (sub,fit_temp)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    
+    if not os.path.isdir(subdirec):
+        os.mkdir(subdirec)
+     
+    os.chdir(subtemp)
+    traj = md.load("traj.xtc",top="Native.pdb")
+    
+    os.chdir(subdirec)
+    FRETr = md.compute_distances(traj,residues, periodic=False)
+    maxvalue = int(np.amax(FRETr)/spacing) + 1
+    minvalue = int(np.amin(FRETr)/spacing)
+    num_bins = maxvalue - minvalue
+    ran_size = (minvalue*spacing,maxvalue*spacing)
+    hist, edges = np.histogram(FRETr,num_bins,ran_size)
+    hist = hist/(np.sum(hist)*spacing)
+    np.savetxt("simf_valuesT%d.dat"%fit_temp,hist)
+    edges = edges + (0.5*spacing)
+    bincenters = edges[:-1]
+    bincenters = np.arange((minvalue*spacing)+(spacing/2.0), maxvalue*spacing, spacing)
+    np.savetxt("simf_centers%d.dat"%fit_temp,bincenters)
+    np.savetxt("simf-params%d.dat"%fit_temp,np.array([num_bins,minvalue*spacing,maxvalue*spacing,spacing]))
+    
     os.chdir(cwd)
+    print "Calculated bins for simulation data at a spacing of %.4f" % spacing
 
-    return sim_feature_avg, sim_feature_err, Jacobian_avg, Jacobian_err
+def get_sim_params(model,fit_temp=def_temp):
+    ##assumes you are in the folder containing the model subdir
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    parmfile = "%s/simf-params%d.dat" % (subdirec,fit_temp)
+    
+    if not os.path.isfile(parmfile):
+        calc_sim_bins(model,fit_temp=fit_temp)  
+    parms = np.loadtxt(parmfile)
+    num_bins = parms[0]
+    ran_size = (parms[1],parms[2])
+    spacing = parms[3]
+    return num_bins, ran_size, spacing
+
+def get_sim_centers(model,fit_temp=def_temp): 
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    simfile = "%s/simf_centers%d.dat" % (subdirec,fit_temp)
+    if not os.path.isfile(simfile):
+        calc_sim_bins(model,fit_temp=fit_temp)
+    return np.loadtxt(simfile)
+
+def get_sim_array(model,fit_temp=def_temp):
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    simfile = "%s/simf_centers%d.dat" % (subdirec,fit_temp)
+    simfilef = "%s/simf_valuesT%d.dat" % (subdirec,fit_temp)
+    if not os.path.isfile(simfile):
+        calc_sim_bins(model,fit_temp=fit_temp)
+    centers = np.loadtxt(simfile)
+    values = np.loadtxt(simfilef)
+    simarray = np.array([centers,values])
+    simarray = np.transpose(simarray)
+    
+    os.chdir(cwd)
+    return simarray
+    
+def fret_hist_calc(model, bin_size, ran_size, spacing):
+    ##read trace file from 
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    FRETfile = "%s/FRET_hist.dat" % subdirec
+    FRETtracefile = "%s/FRET_trace.dat" % cwd
+    
+    ftrace = np.loadtxt(FRETtracefile)
+    hist, edges = np.histogram(ftrace,bin_size,ran_size)
+    #normalize and find bin centers
+    hist = hist/(np.shape(ftrace)[0]*spacing)
+    edges = edges + (0.5*spacing)
+    edges = edges[:-1]
+    bincenters = np.arange(ran_size[0]+(spacing/2.0), ran_size[1], spacing)
+    datas = np.array([bincenters,hist])
+    datas = np.transpose(datas)
+    np.savetxt(FRETfile, datas)
+    
+    print "Rebinned FRET_hist_calc Data"
+
+def check_exp_data(FRETdata, bin_centers):
+    terms = np.shape(FRETdata)[0]
+    i = 0
+    recalc = False
+    ##Verify that the bins line up 
+    while (not recalc) and i<terms:
+        if not FRETdata[i] == bin_centers[i]:
+            recalc = True
+        i += 1
+        print "for iteration i = %d, the recalc value is = %s" % (i, str(recalc))
+    return recalc
+
+def add_error_log(note):
+    errfile = "error_log-JC.txt"
+    if not os.path.isfile(errfile):
+        f = open("error_log-JC.txt","w")
+        f.write("Error Log for This run\n\n")
+        f.write("Global variables are:\n")
+        f.write("Gas constant in kJ per mol = %d\n" % GAS_CONSTANT_KJ_MOL)
+        f.write("pairs used are = " + str(FRET_pairs) + "\n")
+        f.write("Temperature for Fitting used is T = %d\n" % def_temp)
+        f.write("Spacing in FRET pair distance used is = %d\n" %defspacing)
+        f.write("\n")
+        f.write(note)
+        f.write("\n")
+        f.close()
+    else:
+        f = f = open("error_log-JC.txt","a")
+        f.write("\n")
+        f.write(note)
+        f.write("\n")
+        f.close()
+    print "ERROR: CHECK LOG \n"
+
+        
+def get_target_feature(model,fit_temp=def_temp):
+    """ Get target features """
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    subdirec = "%s/fitting_%d" % (sub,iteration)
+    simfile = "%s/simf_centers%d.dat" % (subdirec,fit_temp)
+    FRETfile = "%s/FRET_hist.dat" % subdirec
+    FRETtracefile = "%s/FRET_trace.dat" % sub
+    
+    bin_centers = get_sim_centers(model)
+    bin_size, ran_size, spacing = get_sim_params(model)
+   
+    if not os.path.isfile(FRETfile):
+        fret_hist_calc(model, bin_size, ran_size, spacing)  
+    FRETdata = np.loadtxt(FRETfile)  
+    
+    print FRETdata[:,0]
+    print bin_centers
+    if check_exp_data(FRETdata[:,0], bin_centers):
+        print "Mismatched experimental data and simulated data. Attempting Re-binning"
+        fret_hist_calc(model, bin_size, ran_size, spacing)
+        if check_exp_data(FRETdata[:,0], bin_centers):
+            add_error_log("Catastrophic miscalculation of FRET bins")
+        
+    target = FRETdata[:,1]
+    target_err = FRETdata[:,1]**0.5 ##for lack of a better way, take sqrt of bins for error estimate
+    
+    return target, target_err
+
+def calculate_average_Jacobian(model,residues=FRET_pairs):
+    """ Calculate the average feature vector (ddG's) and Jacobian """
+    cwd = os.getcwd()
+    subdir = model.subdir
+    iteration = model.Mut_iteration
+    sub = "%s/%s/Iter_%d" % (cwd,subdir,iteration)
+    
+    os.chdir(sub)
+    os.chdir("%d_0" % def_temp)
+    beta = 1.0 * (GAS_CONSTANT_KJ_MOL*float(def_temp))
+    
+    print "Computing Jacobian and Simparams for the temperature %d, with spacing %d" % (def_temp, defspacing)
+    Jacobian, simparams = compute_Jacobian_for_directory(model,beta,FRET_pairs,defspacing)
+    
+    os.chdir(cwd)
+    if not np.array_equal(simparams, get_sim_array(model)):
+        add_error_log("Catastrophic error. Simparams from compute_Jacobian_for_directory is different from get_sim_params")
+    
+    sim_feature = simparams[:,1]
+    sim_feature_err = sim_feature ** 0.5
+    Jacobian_err = np.zeros(np.shape(Jacobian))
+    
+    return sim_feature, sim_feature_err, Jacobian, Jacobian_err
 
 def compute_Jacobian_for_directory(model,beta,residues,spacing):
     """ Calculates the Jacobian for one directory """
@@ -153,14 +244,13 @@ def compute_Jacobian_for_directory(model,beta,residues,spacing):
     print "Now preparing the Jacobians"
     
     ##Find the fret Trajectories largest and smallest value
-    maxvalue = int(np.amax(FRETr)/0.1) + 1
-    minvalue = int(np.amin(FRETr)/0.1)
-    
+    maxvalue = int(np.amax(FRETr)/spacing) + 1
+    minvalue = int(np.amin(FRETr)/spacing)
+    total_traj = np.shape(rij)[0]
     num_bins = maxvalue - minvalue
     
     Jacobian = np.zeros((num_bins,model.n_contacts),float)
     Fr = np.zeros(num_bins, float)  ##Number of counts of f for that r
-    total_traj = 100001
     Qr = np.zeros((num_bins,model.n_contacts), float)
     Qcount = np.zeros(num_bins, int)  ##Total value of Qs for that r
     Qavg = np.zeros(model.n_contacts)   ##Average value of Q over whole trajectory 
@@ -208,15 +298,14 @@ def compute_Jacobian_for_directory(model,beta,residues,spacing):
             Jacobian[countr,counte] += i*j
             counte += 1
         countr =+ 1
-    Jacobian *= beta
-    print "Finished the Jacobian"
+    Jacobian *= -1 * beta
+
     
     xFRET = np.arange((minvalue*spacing)+(spacing/2.0), maxvalue*spacing, spacing)
     simparams = np.array([xFRET,Fr])
     simparams = np.transpose(simparams)
-    print simparams
-    print np.shape(simparams)
-
+    
+    print "Finished computing the Jacobian and Simparams"
     return Jacobian, simparams
 
 if __name__ == "__main__":    
