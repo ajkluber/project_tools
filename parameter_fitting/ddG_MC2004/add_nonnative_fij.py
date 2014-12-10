4d8488a4156ca87c161194d98908de93c38f7dc9
Alexander Kluber <ajkluber@rice.edu>
2014-12-09 22:54:35 -0600

    nonnative fij

--- parameter_fitting/ddG_MC2004/add_nonnative_fij.py
+++ parameter_fitting/ddG_MC2004/add_nonnative_fij.py
@@ -4,9 +4,10 @@ import glob
 
 all_fij = glob.glob("fij*dat")
 
-#for i in range(len(all_fij)):
-for i in [0]:
+for i in range(len(all_fij)):
+    print " writing mutation %d" % i
     fij_temp = np.loadtxt(all_fij[i])
     indices = np.nonzero(fij_temp)
     new_fij = np.ones(fij_temp.shape,float)
     new_fij[indices] = fij_temp[indices]
+    np.savetxt(all_fij[i],new_fij)
