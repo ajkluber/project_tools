5d3b5c4f81a97f4a29fe9d8e83883b63199a63ad
Justin Chen <jchen978@gmail.com>
2015-02-10 13:50:51 -0600

    bugs

--- parameter_fitting/FRET/save_new_parameters.py
+++ parameter_fitting/FRET/save_new_parameters.py
@@ -14,7 +14,7 @@ def save(model,soln_index):
     cwd = os.getcwd()
     eps0 = model.pairwise_strengths
     deps = np.loadtxt("xp_%d.dat" % soln_index)
-    fitit = 0
+    fitit = 1
     
     ##calculate the scaling based upon what would actually happen: if it's already at 0.01, and goes more negative, now it effectively shows a deps there of 0, and not some arbitrarily large number
     neweps_effective = eps0 + deps
