9ad9e869cbca3e7eec2ee8117005dcc39eb5e96c
Alexander Kluber <ajkluber@rice.edu>
2015-02-16 18:10:37 -0600

    save_new_model_parameters in ddG can treat nonative interactions special

--- parameter_fitting/ddG_MC2004/save_new_parameters.py
+++ parameter_fitting/ddG_MC2004/save_new_parameters.py
@@ -6,7 +6,7 @@
 import numpy as np
 import os
 
-def save(model,soln_index):
+def save(model,soln_index,nonnative=False):
     ''' Save new parameters '''
 
     # Only the fitting_params (a subset of model_params) are
@@ -41,7 +41,10 @@ def save(model,soln_index):
     #    pass 
 
     # Update parameters
-    model.update_model_param_values(neweps_p)
+    if nonnative:
+        update_model_param_values_nonnative(neweps_p)
+    else:
+        model.update_model_param_values(neweps_p)
 
     cwd = os.getcwd()
     relpath = cwd.split("%s/" % model.path)[1]
@@ -50,3 +53,29 @@ def save(model,soln_index):
     model.pairwise_params_file_location = "%s/pairwise_params" % relpath
     model.model_params_file_location = "%s/model_params" % relpath
 
+
+
+def update_model_param_values_nonnative(model,new_model_param_values):
+    """ If parameter changed sign, change the pairwise interaction type """
+    # Switching between different interaction function types
+    potential_type_switch = {2:3,3:2,4:5,5:4}
+
+    # Loop over fitting_params only 
+    for i in range(model.n_fitting_params):
+        p_idx = model.fitting_params[i]
+        p_pairs = model.model_param_interactions[p_idx]
+        for n in range(len(p_pairs)):
+            if new_model_param_values[i] < 0.:
+                # If parameter changes sign then the pairwise_type is flipped.
+                if model.pairwise_type[p_pairs[n]] == 1:
+                    model.pairwise_type[p_pairs[n]] = 2
+                else:
+                    model.pairwise_type[p_pairs[n]] = potential_type_switch[model.pairwise_type[p_pairs[n]]]
+            else:
+                if model.pairwise_type[p_pairs[n]] == 1:
+                    model.pairwise_type[p_pairs[n]] = 2
+            # Model parameters are always positive
+            model.model_param_values[p_idx] = abs(new_model_param_values[i])   
+
+    # Refresh everything that depends on model parameters
+    model._set_nonbonded_interactions()
