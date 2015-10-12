a684a55d0b918e739e21f1a09e6f9266593a4d19
Chen <jc49@login1.davinci.rice.edu>
2015-10-12 17:37:24 -0500

    fixed bug in using the simplify_lambda option for parameter fitting

--- parameter_fitting/__init__.py
+++ parameter_fitting/__init__.py
@@ -128,7 +128,7 @@ def solve_newtons_method(model,fitopts):
     if fitopts["solver"] == "TSVD_Cplex":
         solver.find_solutions(model,fitopts)
     else:
-        if method == "TSVD":
+        if fitopts["solver"]  == "TSVD":
             solver.find_solutions(model, chosen_cutoffs=fitopts["cutoffs"], simplify=fitopts["simplify_lambdas"])
         else:
             solver.find_solutions(model)
