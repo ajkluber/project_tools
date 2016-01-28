878184cfd3fd7d572d8b01bcac39943c7285bbe3
fyrazu <fyrazu@gmail.com>
2016-01-28 16:51:42 -0600

    Added frame.gro to parallel equilibrium runs

--- simulation/smog_AA_constant_temp.py
+++ simulation/smog_AA_constant_temp.py
@@ -371,6 +371,7 @@ def manually_add_equilibrium_runs(model,fitopts,iteration,temps):
                 os.mkdir(simpath)
                 os.chdir(simpath)
                 shutil.copy('../'+closest_dir+'/frame_'+str(simnum)+'.gro','smog.gro')
+                shutil.copy('../'+closest_dir+'/frame_'+str(simnum)+'.gro','frame.gro')
                 print "    Running temperature ", simpath
                 run_constant_temp(model,T,name,nsteps=nsteps,walltime=walltime,queue=queue)
                 os.chdir("..")
